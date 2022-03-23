d3d4ef7407039462e78cb2f0a61dad0aa345e1d1
Igor <4361921+hobroker@users.noreply.github.com>
2022-03-24 00:57:10 +0200

    Add navigation feature

---
+++ src/features/navigation/components/ElevationScroll.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { useScrollTrigger } from '@mui/material';
+
+interface Props {
+  children: React.ReactElement;
+}
+
+const ElevationScroll = ({ children }: Props) => {
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+};
+
+export default ElevationScroll;
