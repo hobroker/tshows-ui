f8ccc160374817412699e84617fc45a1a51b9665
Igor <4361921+hobroker@users.noreply.github.com>
2022-03-24 00:57:48 +0200

    Update Router.tsx

--- src/features/root/components/Router.tsx
+++ src/features/root/components/Router.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../../home/components/Home';
 import Onboarding from '../../onboarding/components/Onboarding';
+import Login from '../../login/components/Login';
 
 const Router = () => (
   <Routes>
     <Route path="/" element={<Home />} />
-    <Route path="onboarding" element={<Onboarding />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/onboarding" element={<Onboarding />} />
   </Routes>
 );
 
