cd79284eff5b77cf868dab50e54cd121f914d8c1
Igor <4361921+hobroker@users.noreply.github.com>
2022-04-04 00:03:23 +0300

    Update PageWrapper.tsx

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -1,21 +1,11 @@
-import React, { ReactNode } from 'react';
 import { styled } from '@mui/material/styles';
-import { Sx } from '../utils/types';
+import { Box } from '@mui/material';
 
-const StyledBox = styled('div')(({ theme }) => ({
+const PageWrapper = styled(Box)(({ theme }) => ({
   marginTop: theme.spacing(8),
   minHeight: `calc(100vh - ${theme.spacing(8)})`,
   display: 'flex',
   justifyContent: 'center',
 }));
-
-interface Props {
-  sx?: Sx;
-  children: ReactNode;
-}
-
-const PageWrapper = ({ children, sx }: Props) => (
-  <StyledBox sx={sx}>{children}</StyledBox>
-);
 
 export default PageWrapper;
