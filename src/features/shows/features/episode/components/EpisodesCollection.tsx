95d070943b4003d00386af2c8b38a23e89367b3e
Igor <4361921+hobroker@users.noreply.github.com>
2022-04-10 02:01:55 +0300

    Fix duplicated Section wrapper

--- src/features/shows/features/episode/components/EpisodesCollection.tsx
+++ src/features/shows/features/episode/components/EpisodesCollection.tsx
@@ -1,6 +1,5 @@
 import { Episode, EpisodeShowFragment } from '../../../../../generated/graphql';
 import TallCardCollection from '../../../components/base/TallCardCollection';
-import Section from '../../../../home/components/Section';
 import TallEpisodeCard from './TallEpisodeCard';
 
 interface Props {
@@ -13,13 +12,11 @@ interface Props {
 }
 
 const EpisodesCollection = ({ episodes, loading }: Props) => (
-  <Section title="Up next to watch">
-    <TallCardCollection loading={loading} scroll>
-      {episodes.map((episode) => (
-        <TallEpisodeCard key={episode.externalId} episode={episode} />
-      ))}
-    </TallCardCollection>
-  </Section>
+  <TallCardCollection loading={loading} scroll>
+    {episodes.map((episode) => (
+      <TallEpisodeCard key={episode.externalId} episode={episode} />
+    ))}
+  </TallCardCollection>
 );
 
 export default EpisodesCollection;
