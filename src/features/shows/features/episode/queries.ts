4ee69b21f0567e90edbba09358a4fd3083f8864d
Igor <4361921+hobroker@users.noreply.github.com>
2022-04-12 00:29:42 +0300

    Refactor queries location

--- src/features/shows/features/episode/queries.ts
+++ src/features/shows/features/episode/queries.ts
@@ -1,35 +1,10 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_LIST_UP_NEXT = gql`
-  query ListUpNext {
-    listUpNext {
-      ...Episode
-    }
-  }
-`;
-
-export const QUERY_LIST_UPCOMING_NEXT = gql`
-  query ListUpcoming {
-    listUpcoming {
-      ...Episode
-    }
-  }
-`;
-
 export const MUTATION_UPSERT_EPISODE = gql`
   mutation UpsertEpisode($episodeId: Int!, $isWatched: Boolean = true) {
     upsertEpisode(input: { episodeId: $episodeId, isWatched: $isWatched }) {
       ...Episode
     }
-  }
-`;
-
-export const EPISODE_SHOW_FRAGMENT = gql`
-  fragment EpisodeShow on PartialShow {
-    externalId
-    name
-    wideImage
-    tallImage
   }
 `;
 
@@ -45,7 +20,10 @@ export const EPISODE_FRAGMENT = gql`
     wideImage
     airDate
     show {
-      ...EpisodeShow
+      externalId
+      name
+      wideImage
+      tallImage
     }
   }
 `;
