3c2a8698f53760756c798b3eee8a6305973c3f5e
Igor <4361921+hobroker@users.noreply.github.com>
2022-04-13 23:21:29 +0300

    Update LoginAlert.tsx

--- src/features/home/components/LoginAlert.tsx
+++ src/features/home/components/LoginAlert.tsx
@@ -3,7 +3,7 @@ import { RoutePath } from '../../router/constants';
 
 const LoginAlert = () => (
   <Alert icon={false} severity="warning">
-    <Typography variant="body1" sx={{ display: 'inline-block' }}>
+    <Typography variant="body1">
       You're browsing anonyomously. You can{' '}
       <Link href={RoutePath.Login}>login</Link> or{' '}
       <Link href={RoutePath.Register}>register</Link> to for the full
