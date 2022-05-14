e4d9bde63e60b6414bb38a5b9898e2c71a4a342a
Igor <4361921+hobroker@users.noreply.github.com>
2022-05-15 01:35:26 +0300

    Adjust stats page

--- src/features/stats/components/page/StatsCalendar.tsx
+++ src/features/stats/components/page/StatsCalendar.tsx
@@ -1,27 +1,33 @@
 import React, { useContext } from 'react';
 import { ResponsiveCalendar } from '@nivo/calendar';
 import { blue, grey } from '@mui/material/colors';
+import { Box } from '@mui/material';
 import { StatsContext } from '../../contexts/StatsContext';
+import Section from '../../../../components/Section';
 
 const StatsCalendar = () => {
   const { calendar } = useContext(StatsContext);
 
-  if (calendar.length < 2) {
+  if (calendar.length === 0) {
     return null;
   }
 
   return (
-    <ResponsiveCalendar
-      data={calendar}
-      margin={{ left: 20 }}
-      from={calendar[0].day}
-      to={calendar[calendar.length - 1].day}
-      emptyColor={grey[200]}
-      monthBorderColor="#ffffff"
-      colors={[blue[100], blue[300], blue[500], blue[700], blue[900]]}
-      dayBorderWidth={2}
-      dayBorderColor="#ffffff"
-    />
+    <Section title="Activity by day" divider>
+      <Box sx={{ height: 200 }}>
+        <ResponsiveCalendar
+          data={calendar}
+          margin={{ left: 20 }}
+          from={calendar[0].day}
+          to={calendar[calendar.length - 1].day}
+          emptyColor={grey[200]}
+          monthBorderColor="#ffffff"
+          colors={[blue[100], blue[300], blue[500], blue[700], blue[900]]}
+          dayBorderWidth={2}
+          dayBorderColor="#ffffff"
+        />
+      </Box>
+    </Section>
   );
 };
 
