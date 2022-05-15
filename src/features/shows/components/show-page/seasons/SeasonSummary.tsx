d40e4afa9688b01a404cfd23ab185f8eb10e04d1
Igor <4361921+hobroker@users.noreply.github.com>
2022-05-15 19:50:42 +0300

    Extract SeasonWatchedButton

--- src/features/shows/components/show-page/seasons/SeasonSummary.tsx
+++ src/features/shows/components/show-page/seasons/SeasonSummary.tsx
@@ -1,14 +1,10 @@
-import React, { MouseEvent, useCallback, useContext } from 'react';
+import React from 'react';
 import Typography from '@mui/material/Typography';
 import { DateTime } from 'luxon';
-import { Box, Button, Divider } from '@mui/material';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import { Box, Divider } from '@mui/material';
 import { Season } from '../../../../../generated/graphql';
 import CustomImage from '../../CustomImage';
-import { UserContext } from '../../../../user/contexts/UserContext';
-import { UserState } from '../../../../user/constants';
-import { ShowPageContext } from '../../../contexts/ShowPageContext';
+import SeasonWatchedButton from './SeasonWatchedButton';
 
 interface Props {
   season: Season;
@@ -24,57 +20,34 @@ const SeasonSummary = ({
     isFullyWatched,
     number,
   },
-}: Props) => {
-  const { userState } = useContext(UserContext);
-  const { toggleSeasonIsFullyWatched } = useContext(ShowPageContext);
-  const onMarkAllAsWatchedClick = useCallback(
-    (e: MouseEvent<HTMLButtonElement>) => {
-      e.stopPropagation();
-      toggleSeasonIsFullyWatched(number);
-    },
-    [number, toggleSeasonIsFullyWatched],
-  );
-
-  return (
-    <Box sx={{ display: 'flex', gap: 1 }}>
-      <CustomImage path={tallImage} sx={{ width: 80 }} />
-      <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
-        <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-          <Typography variant="body1">{name}</Typography>
-          <Divider orientation="vertical" flexItem />
-          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-            {DateTime.fromISO(airDate).toFormat('d MMM, yyyy')}
-          </Typography>
-          <Divider orientation="vertical" flexItem />
-          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-            {episodeCount} episodes
-          </Typography>
-          {userState === UserState.LoggedIn && (
-            <Button
-              variant="outlined"
-              sx={{ ml: 'auto', lineHeight: 1 }}
-              size="small"
-              onClick={onMarkAllAsWatchedClick}
-              endIcon={
-                isFullyWatched ? (
-                  <CheckCircleIcon />
-                ) : (
-                  <CheckCircleOutlineIcon />
-                )
-              }
-            >
-              {isFullyWatched ? 'Mark as unwatched' : 'Mark as watched'}
-            </Button>
-          )}
-        </Box>
-        <Box>
-          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-            {description}
-          </Typography>
+}: Props) => (
+  <Box sx={{ display: 'flex', gap: 1 }}>
+    <CustomImage path={tallImage} sx={{ width: 80 }} />
+    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+        <Typography variant="body1">{name}</Typography>
+        <Divider orientation="vertical" flexItem />
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          {DateTime.fromISO(airDate).toFormat('d MMM, yyyy')}
+        </Typography>
+        <Divider orientation="vertical" flexItem />
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          {episodeCount} episodes
+        </Typography>
+        <Box sx={{ ml: 'auto' }}>
+          <SeasonWatchedButton
+            seasonNumber={number}
+            isFullyWatched={isFullyWatched}
+          />
         </Box>
       </Box>
+      <Box>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          {description}
+        </Typography>
+      </Box>
     </Box>
-  );
-};
+  </Box>
+);
 
 export default SeasonSummary;
---
+++ src/features/shows/components/show-page/seasons/SeasonWatchedButton.tsx
@@ -0,0 +1,44 @@
+import React, { MouseEvent, useCallback, useContext } from 'react';
+import { Button } from '@mui/material';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import { UserContext } from '../../../../user/contexts/UserContext';
+import { UserState } from '../../../../user/constants';
+import { ShowPageContext } from '../../../contexts/ShowPageContext';
+
+interface Props {
+  isFullyWatched: boolean;
+  seasonNumber: number;
+}
+
+const SeasonWatchedButton = ({ isFullyWatched, seasonNumber }: Props) => {
+  const { userState } = useContext(UserContext);
+  const { toggleSeasonIsFullyWatched } = useContext(ShowPageContext);
+  const onMarkAllAsWatchedClick = useCallback(
+    (e: MouseEvent<HTMLButtonElement>) => {
+      e.stopPropagation();
+      toggleSeasonIsFullyWatched(seasonNumber);
+    },
+    [seasonNumber, toggleSeasonIsFullyWatched],
+  );
+
+  if (userState !== UserState.LoggedIn) {
+    return null;
+  }
+
+  return (
+    <Button
+      variant="outlined"
+      sx={{ lineHeight: 1 }}
+      size="small"
+      onClick={onMarkAllAsWatchedClick}
+      endIcon={
+        isFullyWatched ? <CheckCircleIcon /> : <CheckCircleOutlineIcon />
+      }
+    >
+      {isFullyWatched ? 'Fully watched' : 'Mark as watched'}
+    </Button>
+  );
+};
+
+export default SeasonWatchedButton;
